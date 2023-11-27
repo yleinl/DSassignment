15612c01fe98408107caebd5ec92d95fe7ac356c
abnerlyt <644044443@qq.com>
2023-11-27 22:17:04 +0100

    删了点没用的东西

--- GCN/GCN_Cora_Distributed.py
+++ GCN/GCN_Cora_Distributed.py
@@ -7,45 +7,6 @@ import os
 from torch_geometric.datasets import Planetoid
 
 import GCN_Cora
-def partition_data(dataset, num_partitions):
-    data = dataset[0]
-    num_nodes = data.num_nodes
-    partition_size = num_nodes // num_partitions
-
-    partitions = []
-    for i in range(num_partitions):
-        start_idx = i * partition_size
-        end_idx = (i + 1) * partition_size if i != num_partitions - 1 else num_nodes
-
-        owned_nodes = torch.arange(start_idx, end_idx, dtype=torch.long)
-        owned_mask = torch.zeros(num_nodes, dtype=torch.bool)
-        owned_mask[owned_nodes] = True
-
-        all_nodes = torch.arange(num_nodes, dtype=torch.long)
-
-        node_mapping = {node.item(): idx for idx, node in enumerate(all_nodes)}
-
-        remapped_edge_index = torch.stack([
-            torch.tensor([node_mapping[node.item()] for node in data.edge_index[0]]),
-            torch.tensor([node_mapping[node.item()] for node in data.edge_index[1]])
-        ], dim=0)
-
-        partition_data = data.clone()
-        partition_data.x = data.x[all_nodes]
-        partition_data.edge_index = remapped_edge_index
-        partition_data.train_mask = data.train_mask[all_nodes]
-        partition_data.val_mask = data.val_mask[all_nodes]
-        partition_data.test_mask = data.test_mask[all_nodes]
-        partition_data.y = data.y[all_nodes]
-        partition_data.num_classes = dataset.num_classes
-
-        partition_data.owned_nodes_mask = owned_mask
-        partition_data.redundant_nodes_mask = ~owned_mask
-
-        partitions.append(partition_data)
-
-    return partitions
-
 
 def partition_data(dataset, num_partitions):
     data = dataset[0]
@@ -110,8 +71,6 @@ def recv_object(src):
 
 def main(rank, world_size):
     dist.init_process_group("gloo", rank=rank, world_size=world_size)
-    # print("world_size", world_size)
-    # print("rank", rank)
     if rank == 0:
         name_data = 'Cora'
         dataset = Planetoid(root= '/tmp/' + name_data, name = name_data)
