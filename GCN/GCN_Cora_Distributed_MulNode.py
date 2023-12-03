f4db744c11c0e9106dd4255f798f7b9955ffd708
DSYS course <dsys2352@fs0.das6.cs.vu.nl>
2023-12-03 20:32:20 +0100

    done missing node feature communication between 2 nodes

--- GCN/GCN_Cora_Distributed_MulNode.py
+++ GCN/GCN_Cora_Distributed_MulNode.py
@@ -2,6 +2,7 @@ import pickle
 import torch
 import torch.distributed as dist
 import os
+import numpy as np
 from torch_geometric.datasets import Planetoid
 from torch_geometric.nn import MessagePassing
 from torch_geometric.utils import degree, add_self_loops
@@ -23,7 +24,7 @@ def partition_data(dataset, num_partitions):
         owned_mask = torch.zeros(num_nodes, dtype=torch.bool)
         owned_mask[owned_nodes] = True
 
-        # 荣誉节点
+        # redundant nodes
         edge_index = data.edge_index
         connected_nodes = torch.unique(edge_index[:, owned_mask[edge_index[0]] | owned_mask[edge_index[1]]])
 
@@ -43,6 +44,8 @@ def partition_data(dataset, num_partitions):
         partition_data.test_mask = data.test_mask[connected_nodes]
         partition_data.y = data.y[connected_nodes]
         partition_data.num_classes = dataset.num_classes
+        partition_data.owned_nodes = owned_nodes
+        partition_data.num_nodes = num_nodes
 
         # 标记拥有的节点和冗余节点
         partition_data.owned_nodes_mask = owned_mask[connected_nodes]
@@ -90,29 +93,73 @@ class GraphConvolution(MessagePassing):
 
 
 class Net(torch.nn.Module):
-    def __init__(self,nfeat, nhid, nclass, dropout):
+    def __init__(self,nfeat, nhid, nclass, dropout, rank, world_size):
         super(Net, self).__init__()
+        
+        self.rank = rank
+        
+        self.world_size = world_size
+        
         self.conv1 = GraphConvolution(nfeat, nhid)
 
         self.conv2 = GraphConvolution(nhid, nclass)
 
         self.dropout=dropout
+    
+    def generate_communication_list(self, num_nodes, nodes_from, owned_nodes):
+        communication_nodes = np.unique([node for node in nodes_from if node not in owned_nodes])
+        
+        requested_nodes_list = []
+        for i in range(self.world_size):
+            nodes = []
+            requested_nodes_list.append(nodes)
+        
+        num_partitions = self.world_size - 1
+        partition_size = num_nodes // num_partitions
+        start_idx = [i * partition_size for i in range(num_partitions)]
+        end_idx = [(i + 1) * partition_size if i != num_partitions - 1 else num_nodes for i in range(num_partitions)]
+        Range = [range(start_idx[i], end_idx[i]) for i in range(num_partitions)]        
+
+        for node in communication_nodes:
+            for i in range(num_partitions):
+                if node in Range[i]:
+                    requested_nodes_list[i + 1].append(node)
+        
+        # print(requested_nodes_lis)
+
+        return requested_nodes_list
+
+    def remap_index(self, requested_nodes_list, owned_nodes):
+        return requested_nodes_list % owned_nodes.shape[0] 
 
     def forward(self, data):
-        x, edge_index = data.x, data.edge_index
+        num_nodes, x, edge_index, owned_nodes = data.num_nodes, data.x, data.edge_index, data.owned_nodes
+        
+        requested_nodes_list =  self.generate_communication_list(num_nodes, edge_index[0], owned_nodes)
 
         req = None
-        # print('model rank {}'.format(rank))
-        if rank == 0:
-            # Send the tensor to process 1
-            req = dist.isend(tensor = x, dst = 1)
-            print('Rank 0 started sending')
-        else:
-            # Receive tensor from process 0
-            req = dist.irecv(tensor = x, src = 0)
-            print('Rank 1 started receiving')
-        req.wait()
-        print('Rank ', rank, ' has data ', x[0])
+        for i in range(1, self.world_size):
+            if(self.rank == i or len(requested_nodes_list[i]) == 0):
+                continue
+            
+            if self.rank == 1:
+                # Send the tensor to process 1
+                send_object(torch.tensor(requested_nodes_list[i]), dst = 2)
+                print('Rank 1 has requested data')
+                requested_nodes_feature = recv_object(src = 2)
+                print('Rank1 has received', requested_nodes_feature)
+            else:
+                # Receive tensor from process 0
+                requested_nodes_list = recv_object(src = 1)
+                print('Rank 2 has received', requested_nodes_list)
+                remapped_idx = self.remap_index(requested_nodes_list, owned_nodes)
+                print('Rank 2 index remapped to ', remapped_idx)
+                send_object(x[remapped_idx], dst = 1)
+                print('Rank2 has sent requested data')
+
+            print('Rank ', self.rank, ' has data ', owned_nodes) 
+            print(owned_nodes.shape, owned_nodes.shape[0])
+            # print('Rank ', self.rank, ' has data ', requested_nodes_list)
 
         x = self.conv1(x, edge_index)
 
@@ -135,27 +182,24 @@ def main(rank, world_size, host_addr_full):
         for dst_rank in range(1, world_size):
             send_object(partitions[dst_rank-1], dst=dst_rank)
             print("data sent to node {}".format(dst_rank))
+        dataset = partitions[0]
     else:
         dataset = recv_object(src=0)
         print("data received on node {} from node 0".format(rank))
         
-    nfeat = dataset.num_node_features
-    nhid = 16
-    nclass = dataset.num_classes
-    dropout = 0.5
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = Net(nfeat, nhid, nclass, dropout).to(device)
-    if('datasets' in str(type(dataset))):
-        dataset = dataset[0]
-    data = dataset.to(device)
-    model.load_state_dict(torch.load('model_epoch_1000_Cora.pth'))
-    model.eval()
-    
-    print('model rank {}'.format(rank))
-    _, pred = model(data).max(dim=1)
-    correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
-    acc = correct / data.test_mask.sum().item()
-    print('Accuracy: {:.4f}'.format(acc))
+        nfeat = dataset.num_node_features
+        nhid = 16
+        nclass = dataset.num_classes
+        dropout = 0.5
+        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        model = Net(nfeat, nhid, nclass, dropout, rank, world_size).to(device)
+        data = dataset.to(device)
+        model.load_state_dict(torch.load('model_epoch_1000_Cora.pth'))
+        model.eval()
+        _, pred = model(data).max(dim=1)
+        correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
+        acc = correct / data.test_mask.sum().item()
+        print('Accuracy: {:.4f}'.format(acc))
 
 if __name__ == "__main__":
     rank = int(os.environ['SLURM_PROCID'])
