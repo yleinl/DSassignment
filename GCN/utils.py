132f0d76896454925d871e7b9cd8160c1dc32cd2
abnerlyt <644044443@qq.com>
2023-12-07 18:48:23 +0100

    删除没用的代码

--- GCN/utils.py
+++ GCN/utils.py
@@ -5,7 +5,6 @@ import time
 import torch
 import torch.distributed as dist
 from community import community_louvain
-from torch_geometric.data import Data
 from torch_geometric.utils import to_networkx
 
 
@@ -19,16 +18,13 @@ def partition_data_prev(dataset, num_partitions):
 
     partitions = []
     for i in range(num_partitions):
-        # 分区内拥有的节点范围
         start_idx = i * partition_size
         end_idx = (i + 1) * partition_size if i != num_partitions - 1 else num_nodes
 
-        # 分区内拥有的节点
         owned_nodes = torch.arange(start_idx, end_idx, dtype=torch.long)
         owned_mask = torch.zeros(num_nodes, dtype=torch.bool)
         owned_mask[owned_nodes] = True
 
-        # redundant nodes
         edge_index = data.edge_index
         connected_edges = owned_mask[edge_index[0]] | owned_mask[edge_index[1]]
         connected_nodes = torch.unique(edge_index[:, owned_mask[edge_index[0]] | owned_mask[edge_index[1]]])
@@ -170,14 +166,12 @@ def remap_data_louvain(data, cluster_nodes):
     new_order = [node for cluster in cluster_nodes for node in cluster]
     old_to_new = {old_idx: new_idx for new_idx, old_idx in enumerate(new_order)}
 
-    # 重排节点特征、标签和掩码
     data.x = data.x[torch.tensor(new_order)]
     data.y = data.y[torch.tensor(new_order)]
     data.train_mask = data.train_mask[torch.tensor(new_order)]
     data.val_mask = data.val_mask[torch.tensor(new_order)]
     data.test_mask = data.test_mask[torch.tensor(new_order)]
 
-    # 重排边索引
     remapped_edge_index = torch.stack([
         torch.tensor([old_to_new[node.item()] for node in edge]) for edge in data.edge_index.t()
     ]).t()
@@ -189,20 +183,16 @@ def partition_data_louvain(dataset, num_partitions):
     data = dataset[0]
     G = to_networkx(data, to_undirected=True)
 
-    # 使用Louvain算法进行图聚类
     partition = community_louvain.best_partition(G)
     num_nodes = data.num_nodes
 
-    # 平均每个分区节点数
     cluster_nodes = [[] for _ in range(num_partitions)]
     for node, cluster_id in partition.items():
         cluster_nodes[cluster_id % num_partitions].append(node)
 
-    # 对群组进行均衡调整
     avg_size = num_nodes // num_partitions
     for cluster in cluster_nodes:
         while len(cluster) > avg_size:
-            # 将多余的节点移到其他群组
             moved_node = cluster.pop()
             target_cluster = min(cluster_nodes, key=len)
             target_cluster.append(moved_node)
@@ -217,20 +207,16 @@ def partition_data_louvain_sampled(dataset, num_partitions):
     data = dataset
     G = to_networkx(data, to_undirected=True)
 
-    # 使用Louvain算法进行图聚类
     partition = community_louvain.best_partition(G)
     num_nodes = data.num_nodes
 
-    # 平均每个分区节点数
     cluster_nodes = [[] for _ in range(num_partitions)]
     for node, cluster_id in partition.items():
         cluster_nodes[cluster_id % num_partitions].append(node)
 
-    # 对群组进行均衡调整
     avg_size = num_nodes // num_partitions
     for cluster in cluster_nodes:
         while len(cluster) > avg_size:
-            # 将多余的节点移到其他群组
             moved_node = cluster.pop()
             target_cluster = min(cluster_nodes, key=len)
             target_cluster.append(moved_node)
@@ -245,73 +231,23 @@ def partition_data_louvain_prev(dataset, num_partitions):
     data = dataset[0]
     G = to_networkx(data, to_undirected=True)
 
-    # 使用Louvain算法进行图聚类
     partition = community_louvain.best_partition(G)
     num_nodes = data.num_nodes
-
-    # 平均每个分区节点数
     cluster_nodes = [[] for _ in range(num_partitions)]
     for node, cluster_id in partition.items():
         cluster_nodes[cluster_id % num_partitions].append(node)
 
-    # 对群组进行均衡调整
     avg_size = num_nodes // num_partitions
     for cluster in cluster_nodes:
         while len(cluster) > avg_size:
-            # 将多余的节点移到其他群组
             moved_node = cluster.pop()
             target_cluster = min(cluster_nodes, key=len)
             target_cluster.append(moved_node)
-    # node_partition_id = torch.zeros(num_nodes, dtype=torch.long)
-    # for index, cluster_node in enumerate(cluster_nodes):
-    #     for i in cluster_node:
-    #         node_partition_id[i] = index + 1
 
     data = remap_data_louvain(data, cluster_nodes)
     data.num_classes = dataset.num_classes
 
     return partition_data_prev(data, num_partitions)
-
-    # partitions = []
-    # for i in range(num_partitions):
-    #     owned_nodes = torch.tensor(cluster_nodes[i], dtype=torch.long)
-    #     owned_mask = torch.zeros(num_nodes, dtype=torch.bool)
-    #     owned_mask[owned_nodes] = True
-    #
-    #     edge_index = data.edge_index
-    #     connected_nodes = torch.unique(edge_index[:, owned_mask[edge_index[0]] | owned_mask[edge_index[1]]])
-    #     node_mapping = {node.item(): idx for idx, node in enumerate(connected_nodes)}
-    #
-    #     # 标记一下所属分区
-    #     node_partition_ids = torch.full((data.num_nodes,), -1, dtype=torch.long)  # 初始化为 -1
-    #     node_partition_ids[connected_nodes] = i
-    #     connected_edges = owned_mask[edge_index[0]] | owned_mask[edge_index[1]]
-    #
-    #     remapped_edge_index = torch.stack([
-    #         torch.tensor([node_mapping[node.item()] for node in edge_index[0] if node.item() in connected_nodes]),
-    #         torch.tensor([node_mapping[node.item()] for node in edge_index[1] if node.item() in connected_nodes])
-    #     ], dim=0)
-    #
-    #     # 拷贝原来的数据特征
-    #     partition_data = data.clone()
-    #     partition_data.x = data.x[connected_nodes]
-    #     partition_data.edge_index = remapped_edge_index
-    #     partition_data.train_mask = data.train_mask[connected_nodes]
-    #     partition_data.val_mask = data.val_mask[connected_nodes]
-    #     partition_data.test_mask = data.test_mask[connected_nodes]
-    #     partition_data.y = data.y[connected_nodes]
-    #     partition_data.num_classes = dataset.num_classes
-    #     partition_data.node_partition_id = node_partition_id
-    #     partition_data.prev_edge_index = edge_index[:, connected_edges]
-    #
-    #     # 标记拥有的节点和冗余节点
-    #     partition_data.owned_nodes_mask = owned_mask[connected_nodes]
-    #     partition_data.redundant_nodes_mask = ~owned_mask[connected_nodes]
-    #     partition_data.node_partition_ids = node_partition_ids[connected_nodes]
-    #
-    #     partitions.append(partition_data)
-
-    return partitions
 
 
 def send_with_timeout(tensor, dst, timeout=3):
@@ -331,7 +267,7 @@ def recv_with_timeout(tensor, src, timeout=3):
             dist.recv(tensor, src=src)
             return True
         except Exception as e:
-            time.sleep(1)  # 等待一段时间后重试
+            time.sleep(1)
     return False
 
 def send_object(obj, dst):
