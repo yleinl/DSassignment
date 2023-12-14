90627eff4e5d0df257d6ce73397d4dd8445ca0ee
JoeySh1 <szy1052219220@gmail.com>
2023-12-14 16:56:18 +0100

    all dataset on GAT, test

--- GAT/GAT_Yelp_Distributed.py
+++ GAT/GAT_Yelp_Distributed.py
@@ -1,13 +1,12 @@
 import numpy as np
 import torch
-import torch.nn as nn
 import torch.nn.functional as F
 import os
 import torch.distributed as dist
-from torch_geometric.data import Data
 from torch_geometric.nn import GATConv
-from torch_geometric.datasets import Planetoid
-import torch_geometric.transforms as T
+from torch_geometric.datasets import Planetoid, Yelp
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import degree, add_self_loops
 from utils import recv_object, send_object, partition_data_louvain as partition_data
 from sampler import sample_data
 
@@ -58,11 +57,42 @@ class Net(torch.nn.Module):
 
         self.conv2 = GATConv(self.nhid*self.in_head, self.nclass, concat=False, heads=self.out_head, dropout=self.dropout)
 
+    def generate_communication_list(self, num_nodes, nodes_from, owned_nodes):
+
+        communication_nodes = np.unique([node for node in nodes_from if node not in owned_nodes])
+
+        requested_nodes_list = []
+        for i in range(self.world_size):
+            nodes = []
+            requested_nodes_list.append(nodes)
+
+        num_partitions = self.world_size - 1
+        partition_size = num_nodes // num_partitions
+        start_idx = [i * partition_size for i in range(num_partitions)]
+        end_idx = [(i + 1) * partition_size if i != num_partitions - 1 else num_nodes for i in range(num_partitions)]
+        Range = [range(start_idx[i], end_idx[i]) for i in range(num_partitions)]
+
+        print('Range')
+        print(Range)
+        for node in communication_nodes:
+            for i in range(num_partitions):
+                if node in Range[i]:
+                    requested_nodes_list[i + 1].append(node)
+
+        print('requested_nodes_list')
+        print(requested_nodes_list)
+
+        return requested_nodes_list
+
+    def remap_index(self, requested_nodes_list, owned_nodes):
+        return requested_nodes_list % owned_nodes.shape[0]
+
     def forward(self, data):
         num_nodes, x, prev_edge_index, owned_nodes = data.num_nodes, data.x, data.prev_edge_index, data.owned_nodes
         sent_nodes = data.sent_nodes
         partition_size = data.partition_size
         node_partition_id = data.node_partition_id
+        edge_index = data.edge_index
 
         for target_partition in range(1, world_size+1):
             sent_partition_nodes = []
@@ -74,12 +104,12 @@ class Net(torch.nn.Module):
                         if other_node not in owned_nodes and node_partition_id[other_node] == target_partition:
                             sent_partition_nodes.append([node % partition_size, target_partition])
             sent_nodes[self.rank].append(sent_partition_nodes)
-        edge_index = data.edge_index
 
         x = F.dropout(x, p=0.6, training=self.training)
         x = self.conv1(x, edge_index)
         x = F.elu(x)
         x = F.dropout(x, p=0.6, training=self.training)
+
         size_send_requests = []
         size_recv_buffers = []
         for target_partition in range(0, world_size):
@@ -146,11 +176,12 @@ def main(rank, world_size, host_addr_full):
     torch.distributed.init_process_group(backend="gloo", init_method=host_addr_full, rank=rank, world_size=world_size)
     print("Hello, I am ", rank)
     if rank == 0:
-        name_data = 'Cora'
-        dataset = Planetoid(root= '/tmp/' + name_data, name = name_data)
-        new_data, partitions = partition_data(dataset, world_size)
+        name_data = 'Yelp'
+        dataset = Yelp(root= '/tmp/' + name_data)
+        dataset = sample_data(dataset, sample_fraction=0.6)
+        new_data, partitions = partition_data_louvain_sampled(dataset, world_size)
         for dst_rank in range(1, world_size):
-            send_object(partitions[dst_rank], dst=dst_rank)
+            send_object(partitions[dst_rank-1], dst=dst_rank)
             print("data sent to node {}".format(dst_rank))
         dataset = partitions[0]
         print("data received on node {} from node 0".format(rank))
@@ -165,9 +196,10 @@ def main(rank, world_size, host_addr_full):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model = Net(nfeat, nhid, in_head, out_head, nclass, dropout, rank, world_size).to(device)
         data = dataset.to(device)
-        model.load_state_dict(torch.load('model_epoch_1000_Cora.pth'))
+        model.load_state_dict(torch.load('model_epoch_3000_Yelp.pth'))
         model.eval()
         _, pred = model(data).max(dim=1)
+        pred = torch.sigmoid(model(data)) > 0.5
         pred = pred[:num_nodes]
         correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
         if data.test_mask.sum().item() != 0:
@@ -183,7 +215,7 @@ def main(rank, world_size, host_addr_full):
         final_pred = torch.cat(all_pred, dim=0)
         final_pred = torch.tensor(final_pred)
         correct = float(final_pred[new_data.test_mask].eq(new_data.y[new_data.test_mask]).sum().item())
-        acc = correct / new_data.test_mask.sum().item()
+        acc = correct / (new_data.test_mask.sum().item() * new_data.y.size(1))
         print('Overall Accuracy: {:.4f}'.format(acc))
     else:
         dataset = recv_object(src=0)
@@ -199,14 +231,15 @@ def main(rank, world_size, host_addr_full):
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         model = Net(nfeat, nhid, in_head, out_head, nclass, dropout, rank, world_size).to(device)
         data = dataset.to(device)
-        model.load_state_dict(torch.load('model_epoch_1000_Cora.pth'))
+        model.load_state_dict(torch.load('model_epoch_3000_Yelp.pth'))
         model.eval()
         _, pred = model(data).max(dim=1)
+        pred = torch.sigmoid(model(data)) > 0.5
         pred = pred[:num_nodes]
         send_object(pred, 0)
         correct = float(pred[data.test_mask].eq(data.y[data.test_mask]).sum().item())
         if data.test_mask.sum().item() != 0:
-            acc = correct / data.test_mask.sum().item()
+            acc = correct / (data.test_mask.sum().item() * data.y.size(1))
             print("The accuracy at rank {} is {}".format(rank, acc))
         else:
             print("Rank {} has no test data".format(rank))
@@ -219,5 +252,4 @@ if __name__ == "__main__":
     host_addr = get_master_addr(os.environ['SLURM_STEP_NODELIST'])
     port = 1234
     host_addr_full = 'tcp://' + host_addr + ':' + str(port)
-
     main(rank, world_size, host_addr_full)
